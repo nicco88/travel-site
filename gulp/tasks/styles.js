d4d6624687d9eb2be08ba746e80b9d9e931c523c
Niccolò <perego.nicco@gmail.com>
2017-10-02 20:01:26 +0800

    done great part of the header, still working on it

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,10 +1,12 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssvars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
-var cssImport = require('postcss-import');
-var mixins = require('postcss-mixins');
+var gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    cssImport = require('postcss-import'),
+    mixins = require('postcss-mixins'),
+    hexrgba = require('postcss-hexrgba')
+;
 
 gulp.task('styles', function() {
   // gulp.src() is an asyncronous function
@@ -12,7 +14,7 @@ gulp.task('styles', function() {
   // now we want to run the styles through postCSS filters
   // postcss is expecting an array
   // cssImport at the very beginning
-  .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+  .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
   // prevent watch task from stopping running when there's a syntax css error
   .on('error', function (errorInfo) {
     console.log(errorInfo.toString());
