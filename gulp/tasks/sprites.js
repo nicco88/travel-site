8e048b1ff6864001c81f8ad14d220dcca96091f7
Niccolò <perego.nicco@gmail.com>
2017-10-02 13:58:13 +0800

    completed the sprite gulp tasks and adjusted in html, css

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,11 +1,14 @@
 var
   gulp = require('gulp'),
-  svgSprite = require('gulp-svg-sprite')
+  svgSprite = require('gulp-svg-sprite'),
+  rename = require('gulp-rename'),
+  del = require('del')
 ;
 
 var config = {
   mode: {
     css: {
+      sprite: 'sprite.svg',
       render: {
         css: {
           template: './gulp/templates/sprite.css'
@@ -15,9 +18,32 @@ var config = {
   }
 };
 
+gulp.task('beginClean', function () {
+  return del(['./app/temp/sprite', './app/assets/images/sprites']);
+});
 
-gulp.task('createSprite', function () {
+gulp.task('createSprite', ['beginClean'], function () {
   return gulp.src('./app/assets/images/icons/**/*.svg')
         .pipe(svgSprite(config))
         .pipe(gulp.dest('./app/temp/sprite/'));
 })
+
+gulp.task('copySpriteGraphic', ['createSprite'], function () {
+  return gulp.src('./app/temp/sprite/css/**/*.svg')
+        .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+gulp.task('copySpriteCSS', ['createSprite'], function () {
+  return gulp.src('./app/temp/sprite/css/*.css')
+        .pipe(rename('_sprite.css'))
+        .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function () {
+  return del('./app/temp/sprite');
+});
+
+// we now want to run the abouve tasks, but the copySpriteCSS should run only after
+// the createSprite has finished, that's why we need to add a middle argument in
+//  copySpriteCSS, which is the tasks it depends on
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
