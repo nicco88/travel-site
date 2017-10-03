b27c3f34ca52f95e2d58bd4474ec194e832f0017
Niccolò <perego.nicco@gmail.com>
2017-10-03 16:50:18 +0800

    improved the mobile nav menu style and functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,20 +1,3 @@
-var
-  $ = require('jquery')
-;
+import MobileMenu from './modules/MobileMenu';
 
-import Person from './modules/Person.js';
-
-class Adult extends Person {
-  payTaxes() {
-    // here we can add everything unique to Jane, using class extends
-    console.log(this.name + " now owes $0 in taxes.");
-  }
-}
-
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
+var mobileMenu = new MobileMenu();
