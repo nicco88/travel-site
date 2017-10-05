65a4b82664c42dc1537c1a04bf082262ea55632e
Niccolò <perego.nicco@gmail.com>
2017-10-05 11:40:56 +0800

    Completed lazy loading of images

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 var path = require('path'); // already included in node
 
 module.exports = {
-  entry: "./app/assets/scripts/App.js",
+  entry: {
+    App: "./app/assets/scripts/App.js",
+    Vendor: "./app/assets/scripts/Vendor.js"
+  },
   output: {
     // path.resolve() creates an absoute path, which is compulsory
     path: path.resolve(__dirname, "./app/temp/scripts"),
-    filename: "App.js"
+    filename: "[name].js"
   },
   module: {
     loaders: [
